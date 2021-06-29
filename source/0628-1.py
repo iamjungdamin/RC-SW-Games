bef725677b73028de2d74a795326f0f862e11423
iamjungdamin <jdm0366@naver.com>
2021-06-29 22:12:14 +0900

    원->달러 추가

--- source/0628-1.py
+++ source/0628-1.py
@@ -1,9 +1,15 @@
 from tkinter import *
 window = Tk()
 
-def process():
+def dollar2won():
     dollar = float(e1.get())
+    e2.delete(0,END)
     e2.insert(0,str(dollar*1127))
+
+def won2dollar():
+    won = float(e2.get())
+    e1.delete(0,END)
+    e1.insert(0,str(won/1127))
 
 l1 = Label(window, text='달러',font='helvetica 16 italic')
 l2 = Label(window, text='원',font='helvetica 16 italic')
@@ -15,12 +21,14 @@ e2 = Entry(window,background='blue',foreground='white')
 e1.grid(row=0,column=1)
 e2.grid(row=1,column=1)
 
-b1 = Button(window, text='달러->원',command=process)
-b2 = Button(window, text='원->달러')
+b1 = Button(window, text='달러->원',command=dollar2won)
+b2 = Button(window, text='원->달러',command=won2dollar)
 b1.grid(row=2,column=0)
 b2.grid(row=2,column=1)
 b1.configure(font='helvetica 12')
-b2['bg'] = 'green'
+b1['bg'] = 'lightyellow'
+b2.configure(font='helvetica 12')
+b2['bg'] = 'lightblue'
 
 #L1 = Label(window, text='김영식', bg='red', fg='white', font='helvetica 16 italic')
 #L2 = Label(window, text='정다민', bg='green', fg='white')
