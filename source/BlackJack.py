647a7b848591e1ce2883f11dff5bfd12bdb810e7
iamjungdamin <jdm0366@naver.com>
2021-07-08 10:11:03 +0900

    Again 기능 추가

--- source/BlackJack.py
+++ source/BlackJack.py
@@ -115,9 +115,21 @@ class BlackJack:
             PlaySound('RC-SW-Games/source/blackjack/sounds/win.wav',SND_FILENAME)
         else:                                               #패
             self.Lstatus['text'] = 'You lost'
-            self.playerMoney += self.betMoeny
             PlaySound('RC-SW-Games/source/blackjack/sounds/wrong.wav',SND_FILENAME)
         
+        #변수 초기화
+        self.betMoeny = 0
+        self.LplayerMoney['text'] = 'You have $'+str(self.playerMoney)
+        self.LbetMoney['text'] = '$'+str(self.betMoeny)
+
+        self.B50.configure(state='disabled', bg='gray')
+        self.B10.configure(state='disabled', bg='gray')
+        self.B1.configure(state='disabled', bg='gray')
+        self.Hit.configure(state='disabled', bg='gray')
+        self.Stay.configure(state='disabled', bg='gray')
+        self.Deal.configure(state='disabled', bg='gray')
+        self.Again.configure(state='active', bg='white')
+
     def pressedHit(self):
         self.nCardsPlayer += 1
         self.hitPlayer(self.nCardsPlayer)
@@ -138,12 +150,29 @@ class BlackJack:
         self.Stay['state'] = 'active'
         self.Stay['bg'] = 'white'
         self.Deal['state'] = 'disabled'
-        self.Deal['bg'] = 'grey'
+        self.Deal['bg'] = 'gray'
         self.Again['state'] = 'disabled'
-        self.Again['bg'] = 'grey'
+        self.Again['bg'] = 'gray'
 
     def pressedAgain(self):
-        pass
+        #카드 라벨 이미지 삭제
+        for i in range (self.dealer.inHand()):
+            self.LcardsDealer[i].configure(bg='green',image='')
+        for i in range (self.player.inHand()):
+            self.LcardsPlayer[i].configure(bg='green',image='')
+        self.LcardsDealer.clear()
+        self.LcardsPlayer.clear()
+
+        #점수 라벨, 결과 라벨 초기화
+        self.LdealerPts['text'] = ''
+        self.LplayerPts['text'] = ''
+        self.Lstatus['text'] = ''
+
+        #버튼 초기화
+        self.B50.configure(state='active',bg='white')
+        self.B10.configure(state='active',bg='white')
+        self.B1.configure(state='active',bg='white')
+        self.Again.configure(state='disabled',bg='gray')
 
     def setupButton(self):
         self.B50 = Button(self.window,text='Bet 50',width=6,height=1,font=self.fontstyle2,command=self.pressedB50)
