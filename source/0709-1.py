d7c6ab6d33258125b8f984158a6c8a5e6498f816
iamjungdamin <jdm0366@naver.com>
2021-07-09 12:03:04 +0900

    테트리스 게임

--- source/0709-1.py
+++ source/0709-1.py
@@ -50,6 +50,14 @@ class TetrisApp:
     def check_collision(self,offset):
         # 예 self.stone = [[0, 2, 2],
         #                  [2, 2, 0]]
+        off_x, off_y = offset
+        for y, row in enumerate(self.stone):
+            for x, val in enumerate(row):
+                if 0 <= y+off_y < self.rows and 0<= x+off_x < self.cols:#유효한 인덱스 검사
+                    if val and self.gridCell[y+off_y][x+off_x]:         #스톤도 비어있지않고 보드도 비어있지않으면
+                        return True
+                else:
+                    return True
 
         return False
 
@@ -57,23 +65,27 @@ class TetrisApp:
     def merge_stone(self):
         # 예 self.stone = [[0, 2, 2],
         #                  [2, 2, 0]]
-        pass
+        for y, row in enumerate(self.stone):
+            for x, val in enumerate(row):
+                if val:
+                    self.gridCell[y+self.stone_y][x+self.stone_x] = val
 
     def new_stone(self):
         #7개 tetris 중에서 랜덤으로 하나 선택
+        self.stone = tetris_shapes[random.randrange(len(tetris_shapes))]
 
         #stone x 위치 = 게임판 중간(self.cols/2) -  stone 의 너비의 절반(self.stone[0]/2)
-
+        self.stone_x = self.cols//2 - len(self.stone[0])//2
         #stone y 위치
+        self.stone_y = 0
 
-        pass
-
+        if self.check_collision((self.stone_x,self.stone_y)):
+            self.gameover = True
 
     def init_game(self):
         #2D 격자 숫자 생성 (0 비어 있는 셀)
         self.gridCell = [[0 for x in range(self.cols)]
                             for y in range(self.rows)]
-
 
         #2D 격자 라벨 생성
         self.board = []
@@ -128,29 +140,57 @@ class TetrisApp:
     def rotate(self):
         # 예 self.stone = [[1, 1, 1],
         #                  [0, 1, 0]]
-        # y좌표는 0, 1, x좌표는 0, 1, 2 즉 0열의 2개, 1열의 2개, 2열의 2개 묶음
-        #rotate_left:     [[1, 0],
+
+        # y좌표는 1, 0, x좌표는 2, 1, 0 즉 2열의 2개, 1열의 2개, 0열의 2개 묶음
+        # rotate_left:    [[1, 0],
         #                  [1, 1],
         #                  [1, 0]]
+        # [self.stone[y][x] for y in range(len(self.stone))] for x in range(len(self.stone[0]-1,-1,-1))]
 
-        #y좌표는 0, 1, x좌표는 2, 1, 0 즉 2열의 2개, 1열의 2개, 0열의 2개 묶음
-        #rotate_right:    [[0, 1],
+        # y좌표는 0, 1, x좌표는 0, 1, 2 즉 0열의 2개, 1열의 2개, 2열의 2개 묶음
+        # rotate_right:   [[0, 1],
         #                  [1, 1],
         #                  [0, 1]]
-        pass
+        # [self.stone[y][x] for y in range(len(self.stone)-1,-1,-1)] for x in range(len(self.stone[0]))]
+
+        # 참고로 전치행렬은
+        # [self.stone[y][x] for y in range(len(self.stone))] for x in range(len(self.stone[0]))]
+
+        self.stone = [ [self.stone[y][x] for y in range(len(self.stone))] for x in range(len(self.stone[0]) -1,-1,-1) ]
+
+        if self.check_collision( (self.stone_x,self.stone_y) ):
+            self.stone = [ [self.stone[y][x] for y in range(len(self.stone)-1,-1,-1)] for x in range(len(self.stone[0]))]
+        else:
+            self.paintGrid()
 
     def check_remove(self):
         # 맨 밑줄에서 부터 rows-1,...0 으로 한줄이 채워졌으면 없애고 self.board 앞에 [0,0,0,0,...0]을 붙임
-        pass
+        i = self.rows-1
+        while i >= 0:
+            if not 0 in self.gridCell[i]:
+                del (self.gridCell[i])
+                self.gridCell = [ [0 for _ in range(self.cols)] ] + self.gridCell
+            else:   #채워지지 않았으면 1 감소 (바로 윗줄 검사)
+                i -= 1
 
     def move_down(self):
-        pass
+        if self.check_collision((self.stone_x,self.stone_y+1)):
+            self.merge_stone()
+            self.new_stone()
+            self.check_remove()
+        else:
+            self.stone_y += 1
+        self.paintGrid()
 
     def move_left(self):
-        pass
+        if not self.check_collision((self.stone_x-1,self.stone_y)):
+            self.stone_x -= 1
+            self.paintGrid()
 
     def move_right(self):
-        pass
+        if not self.check_collision((self.stone_x+1,self.stone_y)):
+            self.stone_x += 1
+            self.paintGrid()
 
     def main(self):
         if not (self.gameover or self.pause):
@@ -165,6 +205,7 @@ class TetrisApp:
         self.pause = False
         self.window = Tk()
         self.window.geometry('%dx%d'%(TILE_SIZE*self.cols*2, TILE_SIZE*self.rows))
+        self.window.resizable(False,False)
         self.window.title('테트리스')
         self.frame = Frame(self.window, width=TILE_SIZE*self.cols,height=TILE_SIZE*self.rows, bg='black')
         self.frame.pack(side='left')
